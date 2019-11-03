8cda8adfa0b6109bd84ddb79808b158f55eb7ff5
mac <djespectro@hotmail.com>
2019-11-03 15:00:47 -0600

    Init

---
+++ src/main/java/com/mac/api/restful/controller/Home.java
@@ -0,0 +1,14 @@
+package com.mac.api.restful.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Home {
+
+    @GetMapping("/")
+    public String sayHello(){
+        return "Hello World!";
+    }
+
+}
